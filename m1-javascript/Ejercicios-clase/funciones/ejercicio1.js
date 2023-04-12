349b129f3e85e0472a1151117fb8f1b60f905e4a
anhi1 <anhiemolina@gmail.com>
2023-04-12 13:03:02 +0200

    función con múltiples argumentos y retorno

--- m1-javascript/Ejercicios-clase/funciones/ejercicio1.js
+++ m1-javascript/Ejercicios-clase/funciones/ejercicio1.js
@@ -5,4 +5,18 @@ que  acepte  un  argumento  nombre  y  muestre
 en  consola un saludo personalizado
 , por ejemplo, "¡Hola, Juan!"
 */
- let saludar = "hola"
+ function saludar(nombre) {
+    console.log(`hola ${nombre}`);
+ }
+ //se puede invocar tantas veces
+ saludar("annie");
+ saludar("paty");
+
+ let username = "betty";
+
+ function isValidUsername(username) {
+    return username.length > 0 && username.length < 50;
+ }
+
+ let valiUser = isValidUsername("ejemplousuario");
+ 
---
+++ m1-javascript/Ejercicios-clase/funciones/ejercicio2.js
@@ -0,0 +1,15 @@
+/*
+Crear una función con múltiples
+argumentos y retorno
+1. Escribe  una  función  llamada  sumar  que 
+ acepte  dos  argumentos  a  y  b,  calcule  su  suma  y
+devuelva el resultado.
+*/
+
+function suma(num1,num2) {
+   let result = num1 + num2;
+   return result;
+}
+
+
+console.log(suma(5,4));
