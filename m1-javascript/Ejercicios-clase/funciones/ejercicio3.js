ad6e1aa14b6d5ba9036dbe3690e3824189a8cca8
anhi1 <anhiemolina@gmail.com>
2023-04-12 13:49:05 +0200

    funcion anonima

---
+++ m1-javascript/Ejercicios-clase/funciones/ejercicio3.js
@@ -0,0 +1,20 @@
+/*
+Crear una función anónima y asignarla a
+una variable
+1. Escribe  una  función  anónima  que  acepte 
+un  número  y  devuelva  su  cuadrado. Asigna esta
+función a una variable llamada calcularCuadrado
+*/
+
+
+let calcularCuadrado = function(numero) {
+    return numero**2;
+};
+
+
+console.log(`el cuadrado de 2: ${calcularCuadrado(3)}`);
+
+
+
+
+
---
+++ m1-javascript/Ejercicios-clase/funciones/ejercicio4.js
@@ -0,0 +1,11 @@
+/*
+ Crear una función de flecha
+1. Escribe una función de flecha 
+llamada multiplicar que acepte dos 
+argumentos a y b, y devuelva
+su producto
+*/
+
+function name(params) {
+    
+}
