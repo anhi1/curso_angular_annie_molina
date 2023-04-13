29cecd36fa55cbbb49a81ca805e9b28cd19755c9
anhi1 <anhiemolina@gmail.com>
2023-04-13 14:23:24 +0200

    agregar

---
+++ m1-javascript/Ejercicios-clase/POO/ejemplo2.1.js
@@ -0,0 +1,13 @@
+class SmartPhone{
+    constructor(cores, ram, isAndroid){
+        this.model = model;
+        this.core = core;
+        this.ram = ram;
+        this.isAndroid = isAndroid;
+    }
+}
+
+//this es asociar al objeto
+
+const phone = new SmartPhone("One Plus 11", 4, 12, true);
+const phone2 =new SmartPhone("xiami redmi")
