cc9ea6e6f0378921ea019eaafb8f91999b9489d0
anhi1 <anhiemolina@gmail.com>
2023-04-13 14:08:02 +0200

    datos

--- m1-javascript/Ejercicios-clase/estructura_datos/ejercicio4.js
+++ m1-javascript/Ejercicios-clase/estructura_datos/ejercicio4.js
@@ -29,3 +29,12 @@ console.log(persona1);
 //agregar un array
 persona1.telefonos = ["662585545", "662580399"];
 console.log(persona1);
+
+//for in (iterar las claves de los objetos)
+//ejemplo jey es igual a clave: apellido, edad, ciudad
+//for : cuando iteramos sobre una estructura de datos utilizando un i(indice) como posicion
+//for of(es para estructura de datos) : para iterar en un array
+//for in(es solo para objetos) :iterar las claves o atributos que tiene un objeto
+//for each: recibe una funcion y ejecuta ese comportamiento sobre cada elemnto de la estrucutra de datos/ no te delvuel nada
+let personas = ["Alan","gloria","Vanesa"]
+personas.forEach(persona => console.log(persona));
---
+++ m1-javascript/POO/ejemplo2.js
@@ -0,0 +1,9 @@
+//sin clase, es una forma de crear un objeto/ instanciar
+
+let computer1 = {
+    fabricante:"ASUS",
+    modelo : "ASSA",
+    ram:8,  
+}
+
+let computer2 =
