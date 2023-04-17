741431eda4f34572092ae8fc7050b1e5a3c3c5d1
anhi1 <anhiemolina@gmail.com>
2023-04-17 13:04:35 -0900

    definicion de atributos

---
+++ m1-javascript/m1-caso-practico/caso_practico_final.js
@@ -0,0 +1,16 @@
+//
+class Tarea {
+    constructor(id, title, description, state, priority, date){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.state = state;
+        this.priority = priority;
+        this.date = date;
+    }
+
+}
+ let tarea1 = new Tarea("234", )
+class ServiceTareas {
+    constructor ()
+}
