9324286eb68bfbff03ef2985064d500848644cb8
anhi1 <anhiemolina@gmail.com>
2023-04-17 04:55:05 -0900

    objeto sin cases

---
+++ m1-javascript/Ejercicios-clase/POO/02.Objeto_sin_clases.js
@@ -0,0 +1,17 @@
+
+let computer1 = {
+    fabricante:"ASUS",
+    modelo : "ASSA",
+    ram:8,  
+}
+
+//ejemplo de como crear objetos sin utilizar CLASES
+
+let computer2 = {
+    fabricante : "MSI",
+    modelo : "Modern 15",
+    ram : 16,
+    sensores: ["acelerómetro", "camara"]
+
+}
+console.log(typeof computer2);
--- m1-javascript/Ejercicios-clase/POO/ejemplo1.2.js
+++
@@ -1,9 +0,0 @@
-//sin clase, es una forma de crear un objeto/ instanciar
-
-let computer1 = {
-    fabricante:"ASUS",
-    modelo : "ASSA",
-    ram:8,  
-}
-
-let computer2 =
