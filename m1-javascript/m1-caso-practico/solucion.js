16db5d6c7304e2557720b3970c641ea3af76f861
anhi1 <anhiemolina@gmail.com>
2023-04-21 10:17:19 -0900

    update2

--- m1-javascript/m1-caso-practico/solucion.js
+++ m1-javascript/m1-caso-practico/solucion.js
@@ -1,59 +1,62 @@
 
-/**
- * CASO PRACTICO INDIVIDUAL
- * 1.Crear clase tarea: constructor con atributos
- * 2. Crear clase TareaService o TareaDatabase que es una clase con métodos para simular una base de datos, dentro tiene
-un array de Tareas
- *
- */
+/*
 
-// Paso1 : crear una clase activity
-class Activity{
-    constructor(id, title, description, content, status, importance, date ){
+CASO PRACTICO INDIVIDUAL
+
+1. Crear clase Tarea: constructor con atributos
+2. Crear clase TareaService o TareaDatabase que es una clase con métodos para simular una base de datos, dentro tiene
+un array de Tareas
+3. Crear objetos de ambas clases y probar el programa
+
+*/
+// Paso 1: Crear una clase Activity
+
+class Activity {
+
+    constructor(id, title, content, status, importance, date) { 
         this.id = id;
         this.title = title;
-        this.description = description;
         this.content = content;
         this.status = status;
-        this.importance= importance;
-        this.date = date; //campo tipo fecha Date
-
+        this.importance = importance;
+        this.date = date; // campo tipo fecha Date
     }
 }
 
-// Paso2: crear una clase con metodos para trabajar con objetos activity
-/* Crear una clase que represente un servicio de tareas que tenga como atributo una estructura de
-   datos de tareas y métodos para gestionar las tareas:
-Operaciones CRUD:create, retrieve(recueprar), update, delete
-• Añadir una nueva tarea
-• Devolver todas las tareas (findAll)
-• Devolver todas las tareas filtrando por estado
-• Devolver todas las tareas filtrando por prioridad
-• Actualizar una tarea (update)
-• Eliminar una tarea (deleteById)
-*/
+// Paso 2: Crear una clase base de datos con métodos para trabajar con objetos Activity
+/*
+Crear una clase que represente un servicio de tareas que tenga como atributo una estructura de
+datos de tareas y métodos para gestionar las tareas:
 
-// TagService
-class ActivityDatabase{
-
-    constructor(){
-        //inicializar una base de datos
-        this.activities = []; // this.activities : atributo   / [] -->estructura de datos //lista del array
-        
-    }
-/**
-* Añade una nueva actividad en el array de actividades en consecuencia el array crece
-* Para cada nueva actividad que se quiere añadir, genera un nuevo id que esté libre
-* Para simular una base de datos, generamos un id (1, 2, 3, ...) único para cada Activity
-  con un bucle for o con la clase Math calcular el id más alto de todos los ids 
-* @param {*} activity objeto de la clase Activity
-* @returns devuelve la actividad con un nuevo id asignado
+    Operaciones CRUD: Create, Retrieve, Update, Delete
+• Añadir una nueva tarea OK
+• Devolver todas las tareas OK
+* Devolver tarea por id OK
+• Devolver todas las tareas filtrando por estado OK
+• Devolver todas las tareas filtrando por prioridad OK
+• Actualizar una tarea
+• Eliminar una tarea
 */
-    addActivity(activity){
-       activity.id = this.#generateNextId2(); //// asigna el nuevo id calculado en el objeto activity antes de guardarlo en el array
-       this.activities.push(activity);
-       return activity;
+class ActivityDatabase {
+
+    constructor() {
+        this.activities = []; // inicializar la base de datos
     }
+
+    /**
+     * Añade una nueva actividad en el array de actividades en consecuencia el array crece
+     * Para cada nueva actividad que se quiere añadir, genera un nuevo id que esté libre
+     * Para simular una base de datos, generamos un id (1, 2, 3, ...) único para cada Activity
+        con un bucle for o con la clase Math calcular el id más alto de todos los ids 
+     * @param {*} activity objeto de la clase Activity
+     * @returns devuelve la actividad con un nuevo id asignado
+     */
+    addActivity(activity) {
+        activity.id = this.#generateNextId2(); // asigna el nuevo id calculado en el objeto activity antes de guardarlo en el array
+        this.activities.push(activity);
+        return activity;
+    }
+
     /**
      * Primero calcula cuál es el id (int) más alto de todos los ids pertenecientes 
      * a los objetos actividad del array activities. 
@@ -62,15 +65,14 @@ class ActivityDatabase{
      * Utiliza for y if 
      * @returns 
      */
-    #generateNextId(){
+    #generateNextId() {
         let maxId = 0;
-        for (const currentActivity of this.activities){ //itera cada currentActivity
-            if(currentActivity.id > maxId)
-            maxId = currentActivity.id;
+        for (const currentActivity of this.activities) {
+            if (currentActivity.id > maxId)
+                maxId = currentActivity.id;
         }
-        return ++maxId; //operador incrmento ++ va la izquierda:primero asigna y luego hace el return(devuelve)
+        return ++maxId; // operador incremento ++ va la izquierda: primero asigna y luego hace el return
     }
-
 
     /**
      * Primero calcula cuál es el id (int) más alto de todos los ids pertenecientes 
@@ -80,36 +82,37 @@ class ActivityDatabase{
      * Utiliza map y Math.max
      * @returns 
      */
-     
-    #generateNextId2(){
-        if(this.activities.length === 0)
-        return 1;
-        //utilizar el metodo map() para obtener un array de ids -extraer el id de cada activity- obtengo un array de ids
-        let ids = this.activities.map(activity => activity.id);
-        console.log(ids);
-        //obtener el id maximo utilizando Math.max()
-        let maxId = Math.max(...ids); // se utiliza el operador..para desglosar el array en elemntos
+    #generateNextId2() {
+
+        if (this.activities.length === 0)
+            return 1;
+
+        // utilizar el método map() para obtener un array de ids. Utilizamos map para extraer el id de cada activity
+        let ids = this.activities.map( activity => activity.id );
+
+        // Obtener el id máximo utilizando Math.max()
+        let maxId = Math.max(...ids); // se utiliza el operador ... para desglosar el array en elementos
         return ++maxId;
 
     }
 
-     /**
+    /**
      * Devuelve el array de actividades. SELECT * FROM activities
      * @returns 
      */
-    finAll (){
+    findAll() {
         return this.activities;
     }
 
-     /**
+    /**
      * Devuelve un objeto activity. SELECT * FROM activities WHERE id = X
      * @param {*} id numero id de la actividad a buscar
      * @returns el objeto actividad si existe o undefined si no se encuentra
      */
-    filterById(id){ //filtrar por ID filterById(1)
-       let results = this.activities.filter(activity => activity.id === id);
-        if(results.length === 1)
-        return results[0];
+    filterById(id) {  // filtrar por ID filterById(1)
+        let results = this.activities.filter(activity => activity.id === id);
+        if (results.length === 1)
+            return results[0];
     }
 
     /**
@@ -117,7 +120,7 @@ class ActivityDatabase{
      * @param {*} status un string que representa un estado de tarea: "EN PROGRESO", "TERMINADO"
      * @returns array de actividades o un array vacío
      */
-    filterByStatus(status){ //devuelve una estructura de datos filterByStatus("En progreso");
+    filterByStatus(status) { //devuelve una estructura de datos filterByStatus("En progreso");
         return this.activities.filter(activity => activity.status === status);
     }
 
@@ -126,38 +129,41 @@ class ActivityDatabase{
      * @param {*} title palabras a buscar en el título del activity
      * @returns array de actividades o un array vacío
      */
-    filterByTitle(){
-        return this.activities.filter(activity => activity.title.include(title));
-         // return this.activities.filter(activity => activity.title === title);
+    filterByTitle(title) { 
+        return this.activities.filter(activity => activity.title.includes(title)); //busca el (parametro = title)
+         //return this.activities.filter(activity => activity.title === title);
     }
 
-     /**
+    
+    /**
      * Actualiza un objeto activity existente en el array de activities
      * @param {*} activity a actualizar con el id correcto y los atributos editados
      * @returns objeto activity actualizado o undefined si no lo encuentra
      */
-    update(activity){ //activity es el objeto
-        let activityIndex = this.activities.findIndex(currentActivity => currentActivity.id === activity.id);
-            if (activityIndex === -1)
+    update(activity) {
+
+        let activityIndex = this.activities.findIndex( // encontrar el objeto con el finIndex
+            currentActivity => currentActivity.id === activity.id
+        );
+        if (activityIndex === -1) // si no lo encuentra 
             return undefined;
 
-        this.activities [activityIndex].title = activity.title;
-        this.activities [activityIndex].status = activity.status;
-        return activity;
-       
+        this.activities[activityIndex].title = activity.title; //modifica el title o  status
+        this.activities[activityIndex].status = activity.status;
+        return activity; // te devuelve el activity actualizado y modificado
     }
 
-    update2(activity){
-        let activityIndex = this.activities.findIndex(
+    update2(activity) {
+
+        let activityIndex = this.activities.findIndex( //buscamos la posicion de objeto, para encontrarlo
             currentActivity => currentActivity.id === activity.id
         );
         if (activityIndex === -1)
             return undefined;
 
-        let updatedAct = {...this.activities[activityIndex] , ...activity};
+        let updatedAct = {...this.activities[activityIndex] , ...activity};// el ...activity remplaza a ...this.activities[activityIndex]
 
         this.activities[activityIndex] = updatedAct;
-
     }
 
     /**
@@ -165,13 +171,13 @@ class ActivityDatabase{
      * DELETE FROM activities WHERE id = X
      * @param {*} id el número id del activity que hay que borrar
      */
+    deleteById(id) {
 
-    deleteById(id){
         let activityIndex = this.activities.findIndex(activity => activity.id === id);
-        
-        if(activityIndex === -1)
+
+        if (activityIndex === -1)
             return;
-        
+
         this.activities.splice(activityIndex, 1);
     }
 
@@ -187,31 +193,20 @@ class ActivityDatabase{
      * Devuelve el número de activities en base de datos
      * @returns 
      */
-    count() {
+    count() { // con el count verificamos la longitud - es una funcion que te dice cuantas filas hay en un tabla
         return this.activities.length;
     }
+
 }
 
+ // ======================================= CREAR OBJETO BASE DE DATOS ==============================
 
-//Paso3: crear objetos y probar los metodos CRUD para simular una base de datos
-
-
-//-----------CREAR OBJETo BASE DE DATOS-----------------
-
-let activityDatabase = new ActivityDatabase();
-//es un objeto que tiene una base de datos
+ let activityDatabase = new ActivityDatabase();
 
 
-//-----------CREAR OBJETOS ACTIVITY-----------------
-let reunionTrabajo = new Activity(
-    undefined,  //id
-    "Reunion Javascript", // title
-    "tartar caso practico", // content
-    "En progreso",  // status
-    "Media", // importance
-    new Date("2023-04-18")  // date
-    );
+  // ======================================= CREAR OBJETOS ACTIVITY ==============================
 
+let reunionTrabajo = new Activity(undefined, "Reunión JavaScript", "tratar caso práctico", "En progreso","Media", new Date("2023-04-18"));
 let running = new Activity(
     undefined,
     "Entrenamiento cardiovascular",
@@ -220,39 +215,43 @@ let running = new Activity(
     "Normal",
     new Date("2023-04-15")
 );
-
 let gym = new Activity(
     undefined,
-    "Entrenamiento cardiovascular",
-    "Press banca",
-    "pendiente",
+    "Entrenamiento musculación",
+    "Press banca", 
+    "Pendiente",
     "Normal",
     new Date("2023-04-14")
-    
 );
 
-// =======AGREGANDO ACIVIDADES EN LA BASE DE DATOS : addActivity =======
 
-reunionTrabajo = activityDatabase.addActivity(reunionTrabajo); // id 1 // variable = objeto creado / metodo/ ---- (lo esta guardando en un array id =1)
+  // ======================================= AÑADIR ACTIVITY A BASE DE DATOS: addActivity ==============================
+
+reunionTrabajo = activityDatabase.addActivity(reunionTrabajo); // id 1
 console.log(reunionTrabajo.id);
+
 
 running = activityDatabase.addActivity(running); // id 2
 console.log(running.id);
 
-gym = activityDatabase.addActivity(gym); // id 3   //(gym) es un objeto creado
+gym = activityDatabase.addActivity(gym); // id 3
 console.log(gym.id);
 
-//============ BUSCAR UNO: filterById =================
 
+
+
+ // ======================================= BUSCAR UNO: filterById ==============================
+/*
 console.log(activityDatabase.filterById(1));
 console.log(activityDatabase.filterById(999));
+
 
 
  // ======================================= BUSCAR POR STATUS: filterByStatus ==============================
 
  console.log(activityDatabase.filterByStatus("Pendiente"));
  console.log(activityDatabase.filterByStatus("No existe"));
-
+*/
 
 
  // ======================================= BUSCAR POR TITULO: filterByTitle ==============================
@@ -261,7 +260,7 @@ console.log(activityDatabase.filterById(999));
  console.log(activityDatabase.filterByTitle("No existe"));
 
 
- 
+ /*
  // ======================================= ACTUALIZAR: upate ==============================
 
 console.log(activityDatabase.filterById(1));
@@ -298,5 +297,4 @@ activityDatabase.deleteAll(); // borra todo
 console.log(activityDatabase.count()); // 0 (0)
 
 
-
-//map: transformar y extrae cosas
+*/
