c30e13804d404270318901320fe66e7b555109db
anhi1 <anhiemolina@gmail.com>
2023-04-22 12:37:10 -0900

    3metodos: add - generate id - findAll

--- m1-javascript/m1-caso-practico/caso_practico_final.js
+++ m1-javascript/m1-caso-practico/caso_practico_final.js
@@ -8,9 +8,63 @@ class Tarea {
         this.priority = priority;
         this.date = date;
     }
+}
 
+
+
+class TareaDatabase{
+    constructor(){
+        this.tareas = [];
+    }
+
+    addTarea(tarea){
+        tarea.id = this.#generateId();
+        this.tareas.push(tarea);
+        return tarea.id;
+    }
+
+    #generateId(){
+        let maxId = 0;
+        for(const currentTarea of this.tareas){
+            if (currentTarea.id > maxId)
+            maxId = currentTarea.id
+        }
+        return ++maxId;
+    }
+
+    findAll(){
+        return this.tareas;
+    }
+    
 }
- let tarea1 = new Tarea("234", )
-class ServiceTareas {
-    constructor ()
-}
+
+//--------- Instanciar objeto de la base de datos -----------//
+
+let tareaDatabase = new TareaDatabase();
+
+//----------Instanciar objetos Tarea ---------(id, title, description, state, priority, date)//
+
+let limpiar = new Tarea(1,"Limpieza", "limpiador", "pendiente", "importante", new Date("2023-05-15"));
+
+let cocinar = new Tarea(2,"Cocina", "cocinero", "resuelto", "normal", new Date("2023-02-16"));
+
+let atencionCliente = new Tarea(3,"Atencion", "servicioclientela", "pendiente", "normal", new Date("2023-03-19"));
+
+
+//-------------- addTarea  ------
+
+limpiar = tareaDatabase.addTarea(limpiar);
+console.log(limpiar);
+
+cocinar = tareaDatabase.addTarea(cocinar);
+console.log(cocinar);
+
+atencionCliente = tareaDatabase.addTarea(atencionCliente)
+console.log(atencionCliente);
+
+
+//-------------- findAll --------------
+let allTareas = tareaDatabase.findAll();
+console.log(allTareas); 
+
+
