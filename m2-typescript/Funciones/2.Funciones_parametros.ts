a266ec739ba62e49523d739a7aef9c6d52be8c7c
anhi1 <anhiemolina@gmail.com>
2023-04-24 12:41:16 -0900

    Funciones-parametros

--- m2-typescript/Funciones/2.Funciones_parametros.ts
+++ m2-typescript/Funciones/2.Funciones_parametros.ts
@@ -4,46 +4,48 @@ function saludarPersona1(firstName:string, age?: number): void{
 }
 
 // si no se especifica el parametro entonces tomara valor  undefined
-saludarPersona1("Persona1");
+// saludarPersona1("Persona1");
+// saludarPersona1("Persona1", 30);
+saludarPersona1("annie");
+saludarPersona1("annie", 30);
 
-saludarPersona1("Persona1", 30);
 
-
-
-// Opcion 2
-
-function saludarPersona2(firstName:string, age?: number): void{
+// OPCION 2 
+function saludarPersona2(firstName: string, age?: number):void{
     if(age === undefined){
         console.log(`${firstName} con edad desconocida`);
         return; // hace un return y se marcha
+        
     }
     console.log(`${firstName} con edad ${age}`);
+    
 }
+saludarPersona2('betty')
+saludarPersona2('betty', 37);
 
-// saludarPersona2("Persona2");
-//saludarPersona2("Persona2",30);
 
-
-//OPCION: Comprobar parametro opcional con operador ternario
-function saludarPersona3(firstName:string, age?: number): void{
+//OPCION 3 : Comprobar parametro opcional con operador ternario ?:
+function saludarPersona3(firstName:string, age?:number):void{
     age === undefined ?
-    console.log(`${firstName} con edad desconocida`):
-    console.log(`${firstName} con edad ${age}`);  
+    console.log(`${firstName} con edad desconocida`) : // es igual a if
+    console.log(`${firstName} con edad ${age}`); // else   
 }
 
-saludarPersona2("Persona3");
-saludarPersona2("Persona3",30);
+saludarPersona3('ana', 30);
 
-// cuando algo es indefinido, si toiene valor le asignas y si no 
-// OPCION 4:OPERADO ?? : Asignar por valor por defecto a variable opcional con operador
+// OPCION 4: Asignar valor por defecto a variable opcional con operador (??)
 
-function saludarPersona4(firstName:string, age?: number): void{
-    age = age ?? 18;
-    console.log(`${firstName} con edad ${age} ?? 18`); // UN VALOR POR DEFECTO   
+function saludarPersona4(firstName: string, age?:number):void{
+    console.log(`${firstName} con edad ${age ?? 18}`);
+    
 }
+saludarPersona4("persona4");
+saludarPersona4("Persona4", 30);
+
+
 
 //A NIVEL DE CODIGO LIMPIO O CLEAN CODE
-// CUANTOS MENOS PARAMETROS TENGA UNA FUNCION MAS FACIL SERA DE
+// CUANTOS MENOS PARAMETROS TENGA UNA FUNCION MAS FACIL SERA DE MANTENER Y TESTEAR
 
 //FUNCIONES PURA: NO MODIFICA LOS PARAMETROS
 //PREDECIBLE: NO CREA EFECTOS SECUNDARIOS: que esa funcion modifica la base de datos
