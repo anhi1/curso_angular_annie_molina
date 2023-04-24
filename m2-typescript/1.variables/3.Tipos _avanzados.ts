7a9cab164f1186ca9dd29f8d15ce655497db976d
anhi1 <anhiemolina@gmail.com>
2023-04-24 07:43:53 -0900

    tipos-avanzados

---
+++ m2-typescript/1.variables/3.Tipos _avanzados.ts
@@ -0,0 +1,15 @@
+// tipos avanzados
+
+let codigo_postal1: string | number = "3304";
+let codigo_postal2: string | number = 28002;
+
+console.log(codigo_postal1);
+console.log(codigo_postal2);
+
+console.log(typeof codigo_postal1 === 'string');
+console.log(typeof codigo_postal2 ==='string'); // false
+console.log(typeof codigo_postal2 === 'number'); //true
+
+
+
+
