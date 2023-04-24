26ba3c491d907eba5c15df84629356da7a2712a3
anhi1 <anhiemolina@gmail.com>
2023-04-24 13:23:51 -0900

    cuenta bancaria

--- m2-typescript/POO/cuenta-bancaria.ts
+++ m2-typescript/POO/cuenta-bancaria.ts
@@ -2,12 +2,17 @@
 
 //id, tittular, saldo
 
-class CuentaBancaria{
+class CuentaBancaria {
     constructor(private id:number,
                 private titular:string,
                 private fechaNacimiento:Date,
                 private saldo:number,
                 private pinAcceso:string){}
+    
+    //metodos
+    public consultarSaldo(): number{
+        return this.saldo;
+    }
 
     public incrementarSaldo(cantidad: number):void{
         //1. Comprobar  cantidad debe ser mayor o igual que 5 euros
@@ -19,7 +24,9 @@ class CuentaBancaria{
         if (!this.esPinValido(pin) || !this.esMayorDeEdad() || cantidad > this.saldo) // con tal que una es verdadero entramos
         return 0; // si el pin es falso te devuelve 0
 
-    
+        this.saldo -=cantidad;
+        return cantidad;
+    }
         /*
         // 1. comprobar que la cantidad no sea superior al saldo, en ese caso retiramos
         if(cantidad > this.saldo){
@@ -29,33 +36,57 @@ class CuentaBancaria{
         }
         this.saldo -= cantidad;
         return cantidad;   
-    }
-    */
+        }
+        */
 
-     private esMayorDeEdad(): boolean{
-        //fecha nacimiento 
-        //fecha actual
+    private esMayorDeEdad(): boolean {
         let birthYear = this.fechaNacimiento.getFullYear();
         let fechaActual = new Date();
         let currentYear = fechaActual.getFullYear();
 
-       return currentYear - birthYear >= 18; //devuelve true es mayor de edad
-     }
+       return currentYear - birthYear >= 18; //devuelve true es mayor de edad   
+    }
 
-     private esPinValido(pin: string):boolean{ //es de tipo string y luego tipo de retorno  es boolean
+    private esPinValido(pin: string): boolean{ //es de tipo string y luego tipo de retorno  es boolean
         return this.pinAcceso === pin;
-     }
-
-    
+    }
+  
 }
-// crear objetos
 
-const cuenta1 = new CuentaBancaria(1, 'PERSONA1', new Date("1990-01-01",),1000,'4552'); 
+// CREAR OBJETOS
+//1.Retirar dinero con pin incorrecto
+
+const cuenta1 = new CuentaBancaria(1, 'PERSONA1', new Date("1990-01-01",), 800, '4552'); 
+console.log(`cuenta1.retirarSaldo(3000,'1111'): ${cuenta1.retirarSaldo(3000, '1111')} `);
 
 //let cantidadRetirada = cuenta1.retirarSaldo(1500);
 //console.log(cantidadRetirada);
 
-let cantidadRetirada = cuenta1.retirarSaldo(300);
+// 2. Retirar dinero siendo MENOR de edad
+const cuenta2 = new CuentaBancaria(2, 'PERSONA2', new Date("2015-01-01"), 800, '4552');
+console.log(`cuenta2.retirarSaldo(500, '4552'): ${cuenta2.retirarSaldo(500, '4552')}`); // 0
+
+
+// 3. Cantidad menor que cero
+const cuenta3 = new CuentaBancaria(3, 'PERSONA3', new Date("2000-01-01"), 800, '4552');
+console.log(`cuenta3.retirarSaldo(-30, '4552'): ${cuenta3.retirarSaldo(-30, '4552')}`); // 0
+
+
+// 4. Cantidad mayor que saldo
+
+const cuenta4 = new CuentaBancaria(4, 'PERSONA4', new Date("2000-01-01"), 800, '4552');
+console.log(`cuenta4.retirarSaldo(1500, '4552'): ${cuenta4.retirarSaldo(1500, '4552')}`); // 0
+
+
+// 5. Escenario OK
+
+const cuenta5 = new CuentaBancaria(5, 'PERSONA5', new Date("2000-01-01"), 800, '4552');
+console.log(`cuenta5.retirarSaldo(500, '4552'): ${cuenta5.retirarSaldo(500, '4552')}`); // 500
+
+// Consultar saldo
+
+console.log(`cuenta5.consultarSaldo(): ${cuenta5.consultarSaldo()}`) // te resta segun lo que has retirado antes
+
 
 
 
