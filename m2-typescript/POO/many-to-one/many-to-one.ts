a912bf82a01f3d043ec9431dcc45f25918b0f314
anhi1 <anhiemolina@gmail.com>
2023-04-25 13:06:54 +0200

    many to one

--- m2-typescript/POO/many-to-one/many-to-one.ts
+++ m2-typescript/POO/many-to-one/many-to-one.ts
@@ -7,7 +7,7 @@ import { ISupermarket } from "./supermarket.model";
 
 
 let mercadona: ISupermarket={
-    id: 0,
+    id: 1,
     brandName: "",
     m2: 0,
     numEmployees: 0,
@@ -36,4 +36,41 @@ let pan: IProduct ={
 mercadona.products.push(yogures);
 mercadona.products.push(pan);
 
+// Crear otro supermercado esta vez pasando los productos directamente
+
+let alimerka: ISupermarket ={ //ESTO ES UNIDIRECCIONAL
+    id: 2,
+    brandName: "Alimerka",
+    m2: 200,
+    numEmployees: 24,
+    products: [
+        {
+            id: 3,
+            title:'platano',
+            price: 9.87,
+            quantity:7,
+        //supermarket opcional
+        },
+        {
+            id: 4,
+            title: "Naranjas",
+            price: 0.87,
+            quantity:600
+        }
+    ]
+}
+//manual
+alimerka.products[0].supermarket = alimerka;
+
+//Asignar supermercado alimerka a los productos alimerka
+
+
+// Asignar supermercado alimerka a los productos alimerka utilizando un for
+
+for (const product of alimerka.products){
+    product.supermarket = alimerka;
+    }
+
+//forEach : recibe otra funcion como parametro
 //
+alimerka.products.forEach(product => product.supermarket = alimerka);
