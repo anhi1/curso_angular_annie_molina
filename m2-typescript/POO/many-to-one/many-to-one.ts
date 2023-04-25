9edeab89072dc3ad04e92affa423c3a003f32880
anhi1 <anhiemolina@gmail.com>
2023-04-25 10:37:01 -0900

    many to one

--- m2-typescript/POO/many-to-one/many-to-one.ts
+++ m2-typescript/POO/many-to-one/many-to-one.ts
@@ -1,76 +1,99 @@
 
 //Crear objetos
 
-
 import { IProduct } from "./product.model";
 import { ISupermarket } from "./supermarket.model";
 
-
-let mercadona: ISupermarket={
+let mercadona:ISupermarket = {
     id: 1,
-    brandName: "",
-    m2: 0,
-    numEmployees: 0,
+    brandName: "Mercadona Industrias SL",
+    m2: 150,
+    numEmployees: 20,
     products: []
 }
 
-let yogures: IProduct ={
+let yogurest: IProduct={
     id: 1,
-    title: "Yogures sabor a kiwi",
+    title: "Yogures sabor kiwi",
     price: 4.99,
     quantity: 6,
-    supermarket: mercadona
+    supermarket: mercadona //Many to one
 }
 
-let pan: IProduct ={
+let pan:IProduct={
     id: 2,
-    title: "pan de pueblo rustico",
+    title: "Pan de pueblo Rústico",
     price: 0.99,
     quantity: 20,
     supermarket: mercadona
 }
-// hacer que la asociacion se bidireccional
-//completar la asociacion bidirecional
-//Desde un producto acceder a un supermercado
-//Desde un supermecado puede acceder a los producto
-mercadona.products.push(yogures);
+
+//completar asociacion bidireccional
+// desde un producto acceder a un supermercado
+mercadona.products.push(yogurest);
 mercadona.products.push(pan);
+console.log(mercadona.products);
 
-// Crear otro supermercado esta vez pasando los productos directamente
 
-let alimerka: ISupermarket ={ //ESTO ES UNIDIRECCIONAL
+//Crear otro supermercado esta vez pasando los productos directamente
+let alimerka:ISupermarket={
     id: 2,
-    brandName: "Alimerka",
+    brandName: "Alimerka es Para todos",
     m2: 200,
     numEmployees: 24,
     products: [
-        {
+        { 
             id: 3,
-            title:'platano',
+            title: "Sandía",
             price: 9.87,
-            quantity:7,
-        //supermarket opcional
+            quantity: 7
+            //supermarket opcional ???
+
         },
         {
-            id: 4,
+            id: 4, 
             title: "Naranjas",
             price: 0.87,
-            quantity:600
+            quantity: 600
         }
     ]
 }
-//manual
-alimerka.products[0].supermarket = alimerka;
 
-//Asignar supermercado alimerka a los productos alimerka
+// Unidireccional: únicamente de un lado de la asociación puedo llegar a la otra clase: 
+// Por ejemplo, desde supermercado puedo llegar a producto, pero desde producto no puedo llegar a supermercado
 
+// Bidireccional: desde ambos lados de la asociación puedo acceder a la otra clase:
+// Por ejemplo: desde supermercado puedo llegar a producto y desde un producto también puedo llegar a un supermercado
 
-// Asignar supermercado alimerka a los productos alimerka utilizando un for
+// manual
+// alimerka.products[0].supermarket = alimerka;
+ //alimerka.products[1].supermarket = alimerka;
+
+// Asignar supermercado alimerka a los productos que están dentro de alimerka utilizando un for
+// for 
 
 for (const product of alimerka.products){
-    product.supermarket = alimerka;
-    }
-
-//forEach : recibe otra funcion como parametro
-//
+    product.supermarket = alimerka;   ////????? como comprobar
+    console.log(product.supermarket);
+}
+ 
+ 
+    
+//forEach
 alimerka.products.forEach(product => product.supermarket = alimerka);
+alimerka.products.forEach(product => console.log(product.supermarket));
+
+
+//PRICES
+let prices = alimerka.products.map(product => product.price)
+console.log(prices);
+
+
+
+   
+    
+    
+    
+    
+
+
