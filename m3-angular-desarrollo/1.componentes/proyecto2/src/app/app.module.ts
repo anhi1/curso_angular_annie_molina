f56169123bbfe7cc6ed95e14c69678b39af4aaff
anhi1 <anhiemolina@gmail.com>
2023-05-03 11:04:53 +0200

    ejemplo1

--- m3-angular-desarrollo/1.componentes/proyecto2/src/app/app.module.ts
+++ m3-angular-desarrollo/1.componentes/proyecto2/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Ejemplo1Component } from './ejemplo1/ejemplo1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ejemplo1Component
   ],
   imports: [
     BrowserModule
