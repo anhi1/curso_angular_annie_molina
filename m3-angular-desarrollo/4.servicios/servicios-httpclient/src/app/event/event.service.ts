2b391b7ea8b2ea20400702d29b75855eb34fb690
anhi1 <anhiemolina@gmail.com>
2023-05-09 14:16:03 +0200

    angular servicios componentes

--- m3-angular-desarrollo/4.servicios/servicios-httpclient/src/app/event/event.service.ts
+++ m3-angular-desarrollo/4.servicios/servicios-httpclient/src/app/event/event.service.ts
@@ -10,8 +10,12 @@ export class EventService {
 
   constructor(private httpClient:HttpClient) { }
 
-  findAll(): Observable<any> {
+  findAll(): Observable<any> { // Observable<HttpResponse>
     return this.httpClient.get(this.url);
-
   }
+
+  findById(id: number):Observable<any>{ //any lee cualquier objeto
+    //https://jsonplaceholder.typicode.com/todos/4 o 1, 2, 3
+    return this.httpClient.get(`${this.url}/${id}`); //conn el get traemos la tarea 1 la traea 2...
+  } //(`${this.url}/${id}`)  --> no puede tener espacios
 }
