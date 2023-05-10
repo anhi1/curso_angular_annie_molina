61b473e8676ef11355a310fb34397cab9f19edd5
anhi1 <anhiemolina@gmail.com>
2023-05-10 11:07:49 +0200

    eventService

--- m3-angular-desarrollo/4.servicios/servicios-httpclient/src/app/event/event.service.ts
+++ m3-angular-desarrollo/4.servicios/servicios-httpclient/src/app/event/event.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IEvent } from './event.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,32 @@ export class EventService {
 
   constructor(private httpClient:HttpClient) { }  //para sacar datos de una url usamos el httpClient y lo importamos en el app.module.ts y en service
 
-  findAll(): Observable<any> { // Observable<HttpResponse>
-    return this.httpClient.get(this.url); //get trae los datos
+  findAll(): Observable<IEvent[]> { // Observable<HttpResponse>
+    return this.httpClient.get<IEvent[]>(this.url); //get trae los datos
   }
 
-  findById(id: number):Observable<any>{ //any lee cualquier objeto
+  findById(id: number):Observable<IEvent>{ //any lee cualquier objeto
     //https://jsonplaceholder.typicode.com/todos/4 o 1, 2, 3
-    return this.httpClient.get(`${this.url}/${id}`); //conn el get traemos la tarea 1 la traea 2...
-  } //(`${this.url}/${id}`)  --> no puede tener espacios
+    return this.httpClient.get<IEvent>(`${this.url}/${id}`); //conn el get traemos la tarea 1 la traea 2...
+  }
+
+  //findAllByCompletedTrue()
+  //con 1 parametro: ?completed=true
+  //con 2 parametros: ?param1=value1&param2=value2
+  findAllByCompletedTrue():Observable<IEvent[]>{
+    return this.httpClient.get<IEvent[]>(`${this.url}?completed=true`)
+  }
+
+  findAllByCompletedFalse():Observable<IEvent[]>{
+    return this.httpClient.get<IEvent[]>(`${this.url}?completed=false`)
+  }
+
+
+  //findAllByCompletedFalse()
+  //save
+  //update
+  //deleteById
+
+
 }
+
