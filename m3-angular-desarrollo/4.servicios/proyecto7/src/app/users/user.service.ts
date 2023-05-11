7b06ead747f4c4dba4ac4bcfbf96b4483b7a0f63
anhi1 <anhiemolina@gmail.com>
2023-05-11 14:28:06 +0200

    guardar

--- m3-angular-desarrollo/4.servicios/proyecto7/src/app/users/user.service.ts
+++ m3-angular-desarrollo/4.servicios/proyecto7/src/app/users/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IUser } from './user.model';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  url:string = "https://jsonplaceholder.typicode.com/users/";
+
+  //crear un objeto
+  httpOptions = {
+    observe:'response' as 'body'
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  findAll(): Observable<HttpResponse<IUser[]>> {
+    return this.httpClient.get<HttpResponse<IUser[]>>(this.url, this.httpOptions);
+  }
 }
+
+
