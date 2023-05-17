d37492160d6af3c33a05f6aa4a7bf2bffb165fc8
anhi1 <anhiemolina@gmail.com>
2023-05-17 12:41:55 +0200

    proyecto9

--- m3-angular-desarrollo/5.Enrutado/proyecto10/src/app/app-routing.module.ts
+++ m3-angular-desarrollo/5.Enrutado/proyecto10/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PhotoDetailComponent } from './photo-detail/photo-detail.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotoGalleryComponent } from './photo-gallery/photo-gallery.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    {
+      path: 'photo-gallery',
+      component: PhotoGalleryComponent
+    },
+    {
+      path: 'photo-detail/:id',
+      component: PhotoDetailComponent
+    },
+    {
+      path: 'photo-form',
+      component: PhotoFormComponent
+    },
+    {
+      path: '', redirectTo:'/photo-gallery', pathMatch:'full' //indica la posicion absoluta '/ pra que tome desde el principio
+
+    },
+    {
+      path: '**', redirectTo:'/photo-gallery', pathMatch:'full'
+
+    }
+
+  ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
