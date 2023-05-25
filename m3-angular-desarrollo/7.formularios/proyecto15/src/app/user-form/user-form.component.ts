c9deb619cc4cfe1465a3c59901bf7c507cc8ad0d
anhi1 <anhiemolina@gmail.com>
2023-05-25 14:26:31 +0200

    user form

--- m3-angular-desarrollo/7.formularios/proyecto15/src/app/user-form/user-form.component.ts
+++ m3-angular-desarrollo/7.formularios/proyecto15/src/app/user-form/user-form.component.ts
@@ -8,10 +8,19 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class UserFormComponent {
   userForm = new FormGroup({
-    email: new FormControl(),
-    fullName: new FormControl(),
-    age: new FormControl(),
-    salary: new FormControl(),
+    email: new FormControl(''),
+    fullName: new FormControl(''),
+    age: new FormControl(18),
+    salary: new FormControl(''),
+    isStudent: new FormControl(false),
+    birthday: new FormControl(''),
+    entryHour: new FormControl(''),
+    bookingDate: new FormControl(null),
+    jobCategory:new FormControl(null),
+    company:new FormControl(''), //nota: no poner null ni dejarlo sin string
+    hobbies: new FormControl([]),
+    bio:new FormControl(''),
+    avatar:new FormControl(null)
 
   });
 
@@ -19,9 +28,31 @@ export class UserFormComponent {
     console.log(this.userForm.get('email')?.value);// ? que no sea nulo al objeto
     console.log(this.userForm.get('fullName')?.value);
     console.log(this.userForm.get('age')?.value);
-    console.log(this.userForm.get('salary')?.value);
-    this.userForm.reset();
+    console.log(this.userForm.get('isStudent')?.value);
+    console.log(this.userForm.get('isStudent')?.value);
+    console.log(this.userForm.get('birthday')?.value);
+    console.log(this.userForm.get('entryHour')?.value);
+    console.log(this.userForm.get('bookingDate')?.value);
+    console.log(this.userForm.get('jobCategory')?.value);
+    console.log(this.userForm.get('company')?.value);
+    console.log(this.userForm.get('hobbies')?.value);
+    console.log(this.userForm.get('bio')?.value);
+    console.log(this.userForm.get('avatar')?.value);
 
+    this.userForm.reset();
+  }
+
+  imageSrc: string | undefined;
+
+  uploadFile(event: Event): void{
+  let target =  event.target as HTMLInputElement; //esta funcion coge el evento
+  if(target.files !== null && target.files.length > 0)
+    let fileImg = target.files[0];
+
+    //Mostrar la imagen del usuario
+    let reader = new FileReader();
+    reader.onload = ev => this.imageSrc = reader.result as string;// que hacer cuando se lea la imagen
+    reader.readAsDataURL(fileImg); //leer la imagen
   }
 
 }
