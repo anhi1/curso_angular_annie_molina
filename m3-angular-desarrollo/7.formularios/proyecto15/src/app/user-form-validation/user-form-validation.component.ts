4e12df4871b5881561c3db30edea1e8112d7b586
anhi1 <anhiemolina@gmail.com>
2023-05-29 14:18:24 +0200

    guardar

--- m3-angular-desarrollo/7.formularios/proyecto15/src/app/user-form-validation/user-form-validation.component.ts
+++ m3-angular-desarrollo/7.formularios/proyecto15/src/app/user-form-validation/user-form-validation.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form-validation',
@@ -11,10 +11,27 @@ export class UserFormValidationComponent {
     fullName: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
     nickName: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]),
+    salary: new FormControl(null, [Validators.required, Validators.min(16500), Validators.max(100000)]),
+    nif: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{8}[A-Z]$')]),
+    phone: new FormControl('', [Validators.pattern('^[679]{1}[0-9]{8}$')]),
+    password: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z0-9$%&/()]{8,20}$')]),
+    passwordConfirm: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z0-9$%&/()]{8,20}$')])
+  }, {validators: this.passwordConfirmValidator});
 
-    //validator personalizado
+  passwordConfirmValidator(control: AbstractControl){
+    if (control.get('password')?.value === control.get('passwordConfirm')?.value)
+      return null;
+      else
+      return {'confirmError':true};
+  }
+  nothWhiteSpacesValidator(control: AbstractControl){
+  let containSpaces = (control.value || '').includes('');
+  if(containSpaces)
+    return{'whitespacesError': true}; //si ocntiene espacios si hay un error
+    else
+    return null; //si no contiene espacios esta bien
+}
 
-  });
 
   save(): void{
     if(this.userForm.valid){
@@ -22,7 +39,7 @@ export class UserFormValidationComponent {
     }else{
       console.log("Formulario incorrecto, tiene errores de validadcion");
     }
-    console.log(this.userForm.get('fullName'));
+    console.log(this.userForm.get('nickName'));
   }
 
 
