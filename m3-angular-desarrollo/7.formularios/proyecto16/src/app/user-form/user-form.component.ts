6a9de01ad0f89ccea3450d28db4acf11965fc947
anhi1 <anhiemolina@gmail.com>
2023-05-30 14:34:54 +0000

    formulario

--- m3-angular-desarrollo/7.formularios/proyecto16/src/app/user-form/user-form.component.ts
+++ m3-angular-desarrollo/7.formularios/proyecto16/src/app/user-form/user-form.component.ts
@@ -14,26 +14,26 @@ interface ICompany {
 export class UserFormComponent {
   hobbies: string[] = ['sports', 'music', 'travel'];
   companies: ICompany[] = [
-    {id: 1, name: "Company"},
-    {id: 2, name: "Company"},
-    {id: 3, name: "Company"},
-    {id: 4, name: "Company"},
+    {id: 1, name: "Company1"},
+    {id: 2, name: "Company2"},
+    {id: 3, name: "Company3"},
+    {id: 4, name: "Company4"},
 
   ];
 
 
   hide = true;
   userForm = new FormGroup({
-  fullName: new FormControl('', [Validators.required]),
-  bio: new FormControl('', [Validators.required, Validators.maxLength(256)]),
-  email: new FormControl('', [Validators.required, Validators.email]),
-  isStudent: new FormControl(false),
-  acceptConditions: new FormControl(false, [Validators.requiredTrue]),
-  password: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z0-9$%&/()]{8,20}$')]),
-  salary: new FormControl(null, [Validators.min(16500), Validators.max(100000)]),
-  department: new FormControl('', [Validators.required]),
-  company: new FormControl('', [Validators.required]),
-  hobbies: new FormControl([])
+    fullName: new FormControl('', [Validators.required]),
+    bio: new FormControl('', [Validators.required, Validators.maxLength(256)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    isStudent: new FormControl(false),
+    acceptConditions: new FormControl(false, [Validators.requiredTrue]),
+    password: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z0-9$%&/()]{8,20}$')]),
+    salary: new FormControl(null, [Validators.min(16500), Validators.max(100000)]),
+    department: new FormControl('', [Validators.required]),
+    company: new FormControl('', [Validators.required]),
+    hobbies: new FormControl([])
   });
 
 
