adf2858a2624f865c4b5c3b2b93bd012f6409653
anhi1 <anhiemolina@gmail.com>
2023-06-12 08:41:48 +0000

    enrutado de autor

--- m3-angular-desarrollo/8.proyecto/proyecto1/src/app/authors/authors.module.ts
+++ m3-angular-desarrollo/8.proyecto/proyecto1/src/app/authors/authors.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthorsRoutingModule } from './authors-routing.module';
-
+import { AuthorDetailComponent } from './author-detail/author-detail.component';
+import {MatListModule} from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthorDetailComponent
+  ],
   imports: [
     CommonModule,
-    AuthorsRoutingModule
+    AuthorsRoutingModule,
+    MatListModule,
+    MatGridListModule,
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule
   ]
 })
 export class AuthorsModule { }
--- m3-angular-desarrollo/8.proyecto/proyecto1/src/app/books/book-list/book-list.component.ts
+++ m3-angular-desarrollo/8.proyecto/proyecto1/src/app/books/book-list/book-list.component.ts
@@ -3,7 +3,8 @@ import { BookService } from '../services/book.service';
 import { IBook } from '../models/book.model';
 import { AuthorService } from 'src/app/authors/services/author.service';
 import { IAuthor } from 'src/app/authors/models/author';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-book-list',
@@ -25,32 +26,41 @@ export class BookListComponent implements OnInit {
   constructor(
     private bookService: BookService,
     private authorService: AuthorService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute, //nos permite navegar
+    private snackbar: MatSnackBar //importr en el books module
+    
+    
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params) => {
+    this.loadBooks();
+  }
+  loadBooks() {
+    this.activatedRoute.params.subscribe(params =>{
       const idString = params['authorId'];
-      if (idString) {  //verifico si exsite un id
-        const id = parseInt(idString, 10);
-        this.bookService.findAllByAuthorId(id).subscribe((data) => (this.books = data));
-      } else {
-        this.bookService.findAll().subscribe((data) => (this.books = data));
+      if(idString){
+        const id = parseInt(idString,10);
+        this.bookService.findAllByAuthorId(id).subscribe(data => this.books =data)
+      }else{
+        this.bookService.findAll().subscribe(data => this.books =data);
       }
     });
+    this.authorService.findAll().subscribe(data => this.authors = data);
 
-    
-    this.authorService.findAll().subscribe((data) => (this.authors = data));
   }
-
-  deleteBook(book: IBook) {}
-  this.bookService.deleteById(book.id).subscribe(response => {//voy ha recibir un arespuesta donde voy ha sacr de satatus
-    next: if(response.status === 200 || response.status === 204){ //next cuando todo va bien
-      console.log("se ha borrado correctamente");
-    }else{
-      console.log("se ha producido un error");
-    
-      
+  
+  deleteBook(book: IBook) {
+    this.bookService.deleteById(book.id).subscribe({
+      next: response => {
+        if (response.status === 200 || response.status === 204) {
+          console.log('Se ha borrado correctamente');
+          this.loadBooks(); // tinee todos los codigos que comprueba la url
+        } else {
+          console.log('Se ha producido un error');
+          this.snackbar.open('se ha producido un error, intentalo mas tarde', 'cerrar', {duration:3000})
+        }
+      },
+      error: error => console.log('se ha producido un error,i intentalo mas tarde', {duration: 3000})
     });
-    error: (error) =>
+  }
 }
