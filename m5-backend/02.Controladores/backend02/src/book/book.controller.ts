4470353a0ac4028da7cf423762ac0165122968a8
anhi1 <anhiemolina@gmail.com>
2023-06-13 10:30:22 +0000

    books

--- m5-backend/02.Controladores/backend02/src/book/book.controller.ts
+++ m5-backend/02.Controladores/backend02/src/book/book.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { IBook } from './book.model';
 
-@Controller('book')
-export class BookController {}
+@Controller('books')
+export class BookController {
+
+    @Get()
+    findAll(): IBook[]{
+        let book1: IBook = {
+            id: 1,
+            title: 'Libro 1',
+            price: 29.29,
+        };
+        let book2: IBook = {
+            id: 2,
+            title: 'Libro 2',
+            price: 29.29,
+        };
+        return[book1, book2]
+    }
+}
