2e5a6f157c107278fd0c4af386f9104db957f4e5
anhi1 <anhiemolina@gmail.com>
2023-06-13 11:02:25 +0000

    guardar

--- m5-backend/02.Controladores/backend02/src/book/book.controller.ts
+++ m5-backend/02.Controladores/backend02/src/book/book.controller.ts
@@ -1,21 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { IBook } from './book.model';
 
 @Controller('books')
 export class BookController {
+  @Get()
+  findAll(): IBook[] {
+    let book1: IBook = {
+      id: 1,
+      title: 'Libro 1',
+      price: 29.29,
+    };
+    let book2: IBook = {
+      id: 2,
+      title: 'Libro 2',
+      price: 29.29,
+    };
+    return [book1, book2];
+  }
 
-    @Get()
-    findAll(): IBook[]{
-        let book1: IBook = {
-            id: 1,
-            title: 'Libro 1',
-            price: 29.29,
-        };
-        let book2: IBook = {
-            id: 2,
-            title: 'Libro 2',
-            price: 29.29,
-        };
-        return[book1, book2]
+  @Get(':id')
+  findById(@Param('id') id: string):IBook {
+    console.log(typeof(id));
+    
+    return {
+        id: parseInt(id, 10), // base decimal
+        title: 'Book',
+        price: 30.0
     }
+  }
 }
+
+
