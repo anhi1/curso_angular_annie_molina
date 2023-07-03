e28ee0a85a8d54df16e41bc46dfbe7723c49b45d
anhi1 <anhiemolina@gmail.com>
2023-07-03 16:56:37 +0000

    email

--- m5-backend/06.FullStack/backend07/src/auth/auth.service.ts
+++ m5-backend/06.FullStack/backend07/src/auth/auth.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { TokenDTO } from './dto/token.dto';
+import { LoginDTO } from './dto/loging.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private userService: UsersService,
+        private jwtService: JwtService
+        ){}
+
+    
+    async login(login: LoginDTO): Promise<TokenDTO> {
+
+        let user = await this.userService.findByEmail(login.email);
+
+        if(!user) 
+            throw new UnauthorizedException('Credenciales incorrectas'); // 401
+
+        if(user.password !== login.password) 
+            throw new UnauthorizedException('Credenciales incorrectas'); // 401
+
+        
+        let payload = {
+            email: user.email,
+            sub: user.id
+        }
+
+        let token: TokenDTO = {
+            token: await this.jwtService.signAsync(payload)
+        }
+
+        return token;
+
+    }
+}
